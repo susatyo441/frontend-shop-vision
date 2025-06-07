883c9dd0a66b3404854a98d0ecf4799533a72d17
Gelorawan Susatyo <susatyo441@gmail.com>
2025-06-07 07:55:58 +0700

    feat: button to capture product

--- src/components/ecommerce/RecentOrders.tsx
+++ src/components/ecommerce/RecentOrders.tsx
@@ -106,9 +106,15 @@ export default function RecentOrders() {
           {/* Tombol Tambah Produk */}
           <Link
             className="w-full sm:w-auto inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-xs sm:text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
+            to={"/capture"}
+          >
+            Deteksi Produk
+          </Link>
+          <Link
+            className="w-full sm:w-auto inline-flex items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-xs sm:text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
             to={"/transactions/create"}
           >
-            + Transaksi Baru
+            Manual Input
           </Link>
         </div>
       </div>
