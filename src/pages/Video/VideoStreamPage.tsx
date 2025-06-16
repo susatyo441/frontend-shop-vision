4252027874772bb5b8f8564e96ed5f17c621b712
susatyo441 <susatyo441@gmail.com>
2025-06-16 15:05:58 +0700

    feat: github action

---
+++ src/pages/Video/VideoStreamPage.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import PageBreadcrumb from "../../components/common/PageBreadCrumb";
+import PageMeta from "../../components/common/PageMeta";
+import CaptureProduct from "./VideoStream";
+
+export default function VideoStreamPage() {
+  const [resetKey, setResetKey] = useState(0);
+
+  const handleResetCapture = () => {
+    setResetKey((prev) => prev + 1);
+  };
+  return (
+    <div>
+      <PageMeta
+        title="React.js Form Elements Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        description="This is React.js Form Elements  Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+      />
+      <PageBreadcrumb pageTitle="Product Detection" />
+      <CaptureProduct key={resetKey} onResetRequest={handleResetCapture} />
+    </div>
+  );
+}
